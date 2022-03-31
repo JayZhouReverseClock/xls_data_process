19b469a943a5af574494fe823b329fbb6abbb9bd
1963268047 <1963268047@qq.com>
2022-03-31 10:20:29 +0800

    v1.0_withoutclass

---
+++ ComCol1/ComCol1.cpp
@@ -0,0 +1,53 @@
+﻿// ComCol1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+int main()
+{
+	ifstream DataFile("C:\\Users\\lihaolin\\Desktop\\24道数据.csv");
+	vector<vector<string>> strarray;
+	string line;
+	vector<string> outstr;
+	while (getline(DataFile, line))
+	{
+		istringstream readstr(line);
+		string str;
+		vector<string> linearray;
+		while (getline(readstr, str, ','))
+		{
+			linearray.push_back(str);
+		}
+		strarray.push_back(linearray);
+		linearray.clear();
+	}
+	for (int j = 0; j < 256; j++)
+	{
+		for (int i = 1; i < 3073; i++)
+		{
+			outstr.push_back(strarray[i][j]);
+		}
+	}
+
+	ofstream outfile("C:\\Users\\lihaolin\\Desktop\\data.csv");
+	for (int i = 0; i < outstr.size(); i++)
+	{
+		outfile << outstr[i] << ',' << endl;
+	}
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
